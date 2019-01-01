86861b8c815468c0b2f4ee5e1a337dc2084beebf
guxal <jonathanacp93@gmail.com>
2019-01-01 12:44:02 +0000

    function add_strings_model complete and new function add_strings_api

--- flaskcli_pkg/controllers/add.py
+++ flaskcli_pkg/controllers/add.py
@@ -12,3 +12,20 @@ def add_strings_model(params):
     new_string = "from models import " + params.get('_nclass') + "\n"
     search_and_add('./models/engine/db_storage.py', search_string, new_string)
     search_and_add('./models/engine/file_storage.py', search_string, new_string)
+
+    search_string = "    CNC = {\n"
+    new_string = "        '" + params.get('<[name-model-capitalize]>') + "': " + params.get('_nclass') + "." + params.get('<[name-model-capitalize]>') + ",\n"
+
+    search_and_add('./models/engine/db_storage.py', search_string, new_string)
+
+    search_string = "        'BaseModel': base_model.BaseModel,\n"
+    new_string = "        '" + params.get('<[name-model-capitalize]>') + "': " + params.get('_nclass') + "." + params.get('<[name-model-capitalize]>') + ",\n"
+    
+    search_and_add('./models/engine/file_storage.py', search_string, new_string)
+
+def add_strings_api(params):
+    """
+    """
+    search_string = "from api.v1.views.index import *  # noqa\n"
+    new_string = "from api.v1.views." + params.get('<[name-api-pluralize]>') + " import *  # noqa\n"
+    search_and_add('./api/v1/views/__init__.py', search_string, new_string)
