fa09b40b1ced67de2400099ce06288d9d2a65ff7
guxal <jonathanacp93@gmail.com>
2020-03-03 07:04:41 +0000

    add main.py file

---
+++ flaskcli_pkg/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is CLI for FLASK
+"""
+import typer
+import creates
+
+app = typer.Typer()
+app.add_typer(creates.app, name="create")
+
+if __name__ == "__main__":
+    app()
