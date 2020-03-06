46e2d423492532ab90a2c268737b78846e0ee1bb
guxal <jonathanacp93@gmail.com>
2020-03-06 16:43:19 +0000

    add template dev export_enviroment.sh

---
+++ flaskcli_pkg/templates/dev/export_enviroment.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script configure global ENV for the project <[name-app]>
+<[app]>_MYSQL_USER=<[app-lower]>_dev
+<[app]>_MYSQL_PWD=<[app-lower]>_dev_pwd
+<[app]>_MYSQL_HOST=localhost
+<[app]>_MYSQL_DB=<[app-lower]>_dev_db
+<[app]>_TYPE_STORAGE=db
+<[app]>_API_PORT=5001
+<[app]>_ENV=test
