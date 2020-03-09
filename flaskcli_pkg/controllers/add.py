0c38753d161a24ae15502f400cffb22494b0811c
monicajaimesc <monikmaja@gmail.com>
2020-03-09 22:01:10 +0000

    add new imports to file_storage.py and db_storage.py

--- flaskcli_pkg/controllers/add.py
+++ flaskcli_pkg/controllers/add.py
@@ -7,4 +7,8 @@ def add_strings_model(params):
     new_string = "from models." + params.get('_nclass') + " import "\
                  + params.get('<[name-model-capitalize]>') + "\n"
     search_and_add('./models/__init__.py', search_string, new_string)
-
+    
+    search_string = "from models import base_model\n"
+    new_string = "from models import " + params.get('_nclass') + "\n"
+    search_and_add('./models/engine/db_storage.py', search_string, new_string)
+    search_and_add('./models/engine/file_storage.py', search_string, new_string)
