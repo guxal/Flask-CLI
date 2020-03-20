db2075158e071819a6b1cb98d6d6eca15d371306
guxal <jonathanacp93@gmail.com>
2020-03-20 21:03:48 +0000

    change folder name

---
+++ dev/alias.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script create alias flaskcli
+alias flaskcli="/home/vagrant/__public__/Flask-CLI/flaskcli_pkg/./main.py"
--- dev/alias_flaskcli.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# This script create alias flaskcli
-alias flaskcli="/home/vagrant/__public__/cliflask/flaskcli_pkg/./main.py"
